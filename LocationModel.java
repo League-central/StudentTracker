ebbb81ee3afa09e3f4e44affc3ca6bd11a903aa3
wavis421 <wendy.avis@jointheleague.org>
2019-08-15 16:47:33 -0700

    Release V5.5: Add LOA option to Current Class; sort Loc Codes

---
+++ LocationModel.java
@@ -0,0 +1,46 @@
+package model;
+
+public class LocationModel implements Comparable<LocationModel> {
+	// Location constants
+	public static final int CLASS_LOCATION_UNKNOWN = 0;
+
+	// Location member variables
+	private int locIdx;
+	private String locCode;
+	private String locName, locNameLong;
+	private String notes;
+
+	public LocationModel(int locIdx, String locCode, String locName, String locNameLong, String notes) {
+		this.locIdx = locIdx;
+		this.locCode = locCode;
+		this.locName = locName;
+		this.locNameLong = locNameLong;
+		this.notes = notes;
+	}
+
+	// Getters
+	public int getLocIdx() {
+		return locIdx;
+	}
+
+	public String getLocCode() {
+		return locCode;
+	}
+
+	public String getLocName() {
+		return locName;
+	}
+
+	public String getLocNameLong() {
+		return locNameLong;
+	}
+
+	public String getNotes() {
+		return notes;
+	}
+	
+	@Override
+	public int compareTo(LocationModel other) {
+		return locCode.compareTo(other.getLocCode());
+	}
+}
